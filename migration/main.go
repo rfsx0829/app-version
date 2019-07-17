71c7223f7d7cf45c47534aaf086b103c9e07a977
rfsx0829 <rfsx0829@163.com>
2019-07-17 11:41:45 +0800

    finish basic function

--- migration/main.go
+++ migration/main.go
@@ -1,17 +1,74 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
 
 	"github.com/rfsx0829/little-tools/migration/transfer"
 )
 
 type bed struct {
 	baseHost string
+	token    string
 }
 
-func (b *bed) Upload(data []byte) error {
-	fmt.Println("upload", string(data))
+func (b *bed) Upload(fileName string, data []byte) (err error) {
+	buf := bytes.NewBufferString("")
+	bodyWriter := multipart.NewWriter(buf)
+
+	if err = bodyWriter.SetBoundary("qweadqweas"); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err = bodyWriter.WriteField("token", b.token); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if _, err = bodyWriter.CreateFormFile("file", fileName); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if _, err = buf.Write(data); err != nil {
+		log.Println(err)
+		return
+	}
+
+	bodyWriter.Close()
+
+	reqReader := io.MultiReader(buf)
+	req, err := http.NewRequest("POST", b.baseHost, reqReader)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	req.Header.Set("Connection", "close")
+	req.Header.Set("Pragma", "no-cache")
+	req.Header.Set("Content-Type", bodyWriter.FormDataContentType())
+
+	req.ContentLength = int64(buf.Len())
+
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if data, err := ioutil.ReadAll(resp.Body); err != nil {
+		log.Println(err)
+	} else {
+		fmt.Println(string(data))
+	}
+
 	return nil
 }
 
@@ -24,8 +81,13 @@ func (d *down) BaseURL() string {
 }
 
 func (d *down) Download(link string) ([]byte, error) {
-	fmt.Println("download", link)
-	return nil, nil
+	resp, err := http.Get(link)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return ioutil.ReadAll(resp.Body)
 }
 
 func main() {
@@ -34,7 +96,8 @@ func main() {
 		panic(err)
 	}
 
-	trans.Uploader = &bed{""}
+	trans.RegExp = "http[s?]://[\\d\\w\\./-]*\\.(jpg|png)"
+	trans.Uploader = &bed{"http://localhost:8000/upload", "token"}
 	trans.Downloader = &down{"jianshu.io"}
 
 	trans.Do()
--- migration/transfer/transfer.go
+++ migration/transfer/transfer.go
@@ -3,13 +3,14 @@ package transfer
 import (
 	"io/ioutil"
 	"log"
+	"os"
 	"regexp"
 	"strings"
 )
 
 // Uploader can upload something
 type Uploader interface {
-	Upload([]byte) error
+	Upload(name string, data []byte) error
 }
 
 // Downloader can download something
@@ -21,6 +22,7 @@ type Downloader interface {
 // Transfer schedule the job
 type Transfer struct {
 	MainText []byte
+	RegExp   string
 
 	Uploader
 	Downloader
@@ -37,20 +39,38 @@ func NewTransWithFilename(fileName string) (trans *Transfer, err error) {
 	return
 }
 
+func (t *Transfer) ChangeFile(newFileName string) (err error) {
+	t.MainText, err = ioutil.ReadFile(newFileName)
+	return
+}
+
 func (t *Transfer) Do() {
-	links := t.findAll()
+	links := t.FindAll()
+
 	for _, e := range links {
-		if data, err := t.Download(string(e)); err != nil {
+		t.Once(string(e))
+	}
+}
+
+func (t *Transfer) Once(link string) {
+	fileName := link[strings.LastIndex(link, "/")+1:]
+
+	if data, err := t.Download(link); err != nil {
+		log.Println(err)
+		log.Println(link)
+	} else if err = t.Upload(fileName, data); err != nil {
+		log.Println(err)
+		log.Println(fileName, "failed. Write in local file.")
+		if err = ioutil.WriteFile(fileName, data, os.ModePerm); err != nil {
 			log.Println(err)
-		} else if err = t.Upload(data); err != nil {
-			log.Println(err)
+			log.Println("WriteFile failed.")
 		}
 	}
 }
 
-func (t *Transfer) findAll() [][]byte {
+func (t *Transfer) FindAll() [][]byte {
 	baseURL := t.BaseURL()
-	results := regexp.MustCompile("https?://\\S+\\.(jpg|png)").FindAll(t.MainText, -1)
+	results := regexp.MustCompile(t.RegExp).FindAll(t.MainText, -1)
 	for i, e := range results {
 		if !strings.Contains(string(e), baseURL) {
 			results = append(results[:i], results[i+1:]...)
